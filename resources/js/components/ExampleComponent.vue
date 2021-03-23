337e1c8f5c7db384b2a83cfb605b767fd031f76c
Murillo Jesus <murillos.jesus@hotmail.com>
2021-03-23 03:21:58 -0300

    Vue 3 Config

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">
+            Example Component
+          </div>
+
+          <div class="card-body">
+            I'm an example component.
+          </div>
+          <new-component />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NewComponent from "./NewComponent.vue"
+
+    export default 
+    {
+        components: { NewComponent },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
